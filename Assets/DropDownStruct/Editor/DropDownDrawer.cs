8a7e4933f5f3ed2d065dfaf1148df174d76600d1
SolidAlloy <tadamori.yoshi@gmail.com>
2020-05-08 21:01:06 +0300

    Add assembly definitions

--- Assets/DropDownStruct/Editor/DropDownDrawer.cs
+++ Assets/DropDownStruct/Editor/DropDownDrawer.cs
@@ -1,170 +1,173 @@
-﻿using System.Linq;
-using System.Reflection;
-using com.spacepuppy.Dynamic;
-using UnityEditor;
-using UnityEngine;
-
-[CustomPropertyDrawer(typeof(DropDownAttribute))]
-public class DropDownDrawer : PropertyDrawer
+﻿namespace DropDownStruct
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    using System.Linq;
+    using System.Reflection;
+    using com.spacepuppy.Dynamic;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(DropDownAttribute))]
+    public class DropDownDrawer : PropertyDrawer
     {
-        var dropDownAttribute = attribute as DropDownAttribute;
-        var fields = dropDownAttribute.Fields;
-
-        if (fields.Length != 0)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            int index = FindByValue(fields, GetTargetObjectOfProperty(property));
-            index = EditorGUI.Popup(position, fields[index].Name, index, GetFieldNames(fields));
+            var dropDownAttribute = attribute as DropDownAttribute;
+            var fields = dropDownAttribute.Fields;
 
-            SetTargetObjectOfProperty(property, fields[index].GetValue(null));
-        }
-    }
-
-    private static int FindByValue(FieldInfo[] fields, object value)
-    {
-        for (int i = 0; i < fields.Length; i++)
-        {
-            if (fields[i].GetValue(null).Equals(value))
+            if (fields.Length != 0)
             {
-                return i;
+                int index = FindByValue(fields, GetTargetObjectOfProperty(property));
+                index = EditorGUI.Popup(position, fields[index].Name, index, GetFieldNames(fields));
+
+                SetTargetObjectOfProperty(property, fields[index].GetValue(null));
             }
         }
 
-        return 0;
-    }
-
-    private static string[] GetFieldNames(FieldInfo[] fields)
-    {
-        return (from FieldInfo field in fields select field.Name).ToArray();
-    }
-
-    /// <summary>
-    /// Gets the object the property represents.
-    /// </summary>
-    /// <param name="prop"></param>
-    /// <returns></returns>
-    private static object GetTargetObjectOfProperty(SerializedProperty prop)
-    {
-        if (prop == null)
+        private static int FindByValue(FieldInfo[] fields, object value)
         {
-            return null;
-        }
-
-        var path = prop.propertyPath.Replace(".Array.data[", "[");
-        object obj = prop.serializedObject.targetObject;
-        var elements = path.Split('.');
-        foreach (var element in elements)
-        {
-            if (element.Contains("["))
+            for (int i = 0; i < fields.Length; i++)
             {
-                var elementName = element.Substring(0, element.IndexOf("["));
-                var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
-                obj = GetValue_Imp(obj, elementName, index);
-            }
-            else
-            {
-                obj = GetValue_Imp(obj, element);
-            }
-        }
-
-        return obj;
-    }
-
-    private static void SetTargetObjectOfProperty(SerializedProperty prop, object value)
-    {
-        var path = prop.propertyPath.Replace(".Array.data[", "[");
-        object obj = prop.serializedObject.targetObject;
-        var elements = path.Split('.');
-        foreach (var element in elements.Take(elements.Length - 1))
-        {
-            if (element.Contains("["))
-            {
-                var elementName = element.Substring(0, element.IndexOf("["));
-                var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
-                obj = GetValue_Imp(obj, elementName, index);
-            }
-            else
-            {
-                obj = GetValue_Imp(obj, element);
-            }
-        }
-
-        if (obj is null)
-        {
-            return;
-        }
-
-        try
-        {
-            var element = elements.Last();
-
-            if (element.Contains("["))
-            {
-                var elementName = element.Substring(0, element.IndexOf("["));
-                var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
-                if (DynamicUtil.GetValue(element, elementName) is System.Collections.IList arr)
+                if (fields[i].GetValue(null).Equals(value))
                 {
-                    arr[index] = value;
+                    return i;
                 }
             }
-            else
-            {
-                DynamicUtil.SetValue(obj, element, value);
-            }
-        }
-        catch
-        {
-            return;
-        }
-    }
 
-    private static object GetValue_Imp(object source, string name)
-    {
-        if (source == null)
-        {
-            return null;
+            return 0;
         }
 
-        var type = source.GetType();
-
-        while (type != null)
+        private static string[] GetFieldNames(FieldInfo[] fields)
         {
-            var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-            if (f != null)
-            {
-                return f.GetValue(source);
-            }
-
-            var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-            if (p != null)
-            {
-                return p.GetValue(source, null);
-            }
-
-            type = type.BaseType;
+            return (from FieldInfo field in fields select field.Name).ToArray();
         }
 
-        return null;
-    }
-
-    private static object GetValue_Imp(object source, string name, int index)
-    {
-        if (!(GetValue_Imp(source, name) is System.Collections.IEnumerable enumerable))
+        /// <summary>
+        /// Gets the object the property represents.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        private static object GetTargetObjectOfProperty(SerializedProperty prop)
         {
-            return null;
-        }
-
-        var enm = enumerable.GetEnumerator();
-
-        for (int i = 0; i <= index; i++)
-        {
-            if (!enm.MoveNext())
+            if (prop == null)
             {
                 return null;
             }
+
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+
+            return obj;
         }
 
-        return enm.Current;
+        private static void SetTargetObjectOfProperty(SerializedProperty prop, object value)
+        {
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements.Take(elements.Length - 1))
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+
+            if (obj is null)
+            {
+                return;
+            }
+
+            try
+            {
+                var element = elements.Last();
+
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty));
+                    if (DynamicUtil.GetValue(element, elementName) is System.Collections.IList arr)
+                    {
+                        arr[index] = value;
+                    }
+                }
+                else
+                {
+                    DynamicUtil.SetValue(obj, element, value);
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private static object GetValue_Imp(object source, string name)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            var type = source.GetType();
+
+            while (type != null)
+            {
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f != null)
+                {
+                    return f.GetValue(source);
+                }
+
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p != null)
+                {
+                    return p.GetValue(source, null);
+                }
+
+                type = type.BaseType;
+            }
+
+            return null;
+        }
+
+        private static object GetValue_Imp(object source, string name, int index)
+        {
+            if (!(GetValue_Imp(source, name) is System.Collections.IEnumerable enumerable))
+            {
+                return null;
+            }
+
+            var enm = enumerable.GetEnumerator();
+
+            for (int i = 0; i <= index; i++)
+            {
+                if (!enm.MoveNext())
+                {
+                    return null;
+                }
+            }
+
+            return enm.Current;
+        }
     }
 }
--- Assets/DropDownStruct/Runtime/DropDownAttribute.cs
+++ Assets/DropDownStruct/Runtime/DropDownAttribute.cs
@@ -1,39 +1,42 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using UnityEngine;
-
-public class DropDownAttribute : PropertyAttribute
+﻿namespace DropDownStruct
 {
-    public DropDownAttribute(Type structType)
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using UnityEngine;
+
+    public class DropDownAttribute : PropertyAttribute
     {
-        var fields = structType.GetFields(BindingFlags.Static | BindingFlags.Public);
-        if (CheckFields(fields, structType))
+        public DropDownAttribute(Type structType)
         {
-            Fields = fields;
+            var fields = structType.GetFields(BindingFlags.Static | BindingFlags.Public);
+            if (CheckFields(fields, structType))
+            {
+                Fields = fields;
+            }
+            else
+            {
+                Fields = new FieldInfo[] { };
+            }
         }
-        else
-        {
-            Fields = new FieldInfo[] { };
-        }
-    }
 
-    public FieldInfo[] Fields { get; private set; }
+        public FieldInfo[] Fields { get; private set; }
 
-    private bool CheckFields(FieldInfo[] fields, Type structType)
-    {
-        Type[] fieldTypes = (from FieldInfo field in fields select field.FieldType).ToArray();
-        if (fieldTypes.Skip(1).All(t => t.Equals(fieldTypes[0])))
+        private bool CheckFields(FieldInfo[] fields, Type structType)
         {
-            return true;
-        }
-        else
-        {
-            Debug.LogErrorFormat(
-                "Public static fields of {0} have inconsistent types. Please make them all of one type.",
-                structType);
+            Type[] fieldTypes = (from FieldInfo field in fields select field.FieldType).ToArray();
+            if (fieldTypes.Skip(1).All(t => t.Equals(fieldTypes[0])))
+            {
+                return true;
+            }
+            else
+            {
+                Debug.LogErrorFormat(
+                    "Public static fields of {0} have inconsistent types. Please make them all of one type.",
+                    structType);
 
-            return false;
+                return false;
+            }
         }
     }
 }
