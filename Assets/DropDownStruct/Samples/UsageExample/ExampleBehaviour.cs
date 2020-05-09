8c484d9137776292ba6a9c1a148ef18cffbb8c63
SolidAlloy <tadamori.yoshi@gmail.com>
2020-05-09 14:23:53 +0300

    Add explanation comments to the sample

--- Assets/DropDownStruct/Samples/UsageExample/ExampleBehaviour.cs
+++ Assets/DropDownStruct/Samples/UsageExample/ExampleBehaviour.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 
-public class ExampleBehaviour : MonoBehaviour
+namespace DropDownStruct
 {
-    [DropDown(typeof(Directions))]
-    [SerializeField] private Vector2 _direction = Directions.Right;
-
-    private void OnValidate()
+    public class ExampleBehaviour : MonoBehaviour
     {
-        Debug.Log(_direction.ToString());
-    }
+        // Specify the struct or class that will act as a drop-down list.
+        // You can also set one of struct's static fields as a default value.
+        [DropDown(typeof(Directions))]
+        [SerializeField] private Vector2 _direction = Directions.Right;
 
-    private struct Directions
-    {
-        public static Vector2 Left = Vector2.left;
-        public static Vector2 Right = Vector2.right;
-        public static Vector2 Up = Vector2.up;
-        public static Vector2 Down = Vector2.down;
+        private void OnValidate()
+        {
+            Debug.Log(_direction.ToString());
+        }
+
+        // Only public static fields will be listed in a drop-down. They must have the same type.
+        // You can have as many private fields and methods as you want. They won't be taken into account.
+        private struct Directions
+        {
+            public static Vector2 Left = Vector2.left;
+            public static Vector2 Right = Vector2.right;
+            public static Vector2 Up = Vector2.up;
+            public static Vector2 Down = Vector2.down;
+        }
     }
 }
