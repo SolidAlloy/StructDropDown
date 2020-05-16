a3f1275297c853c989666dbaca40ba8ca7351df3
SolidAlloy <tadamori.yoshi@gmail.com>
2020-05-16 22:52:40 +0300

    Add documentation to the attribute

--- Assets/StructDropDown/Runtime/DropDownAttribute.cs
+++ Assets/StructDropDown/Runtime/DropDownAttribute.cs
@@ -5,11 +5,26 @@
     using System.Reflection;
     using UnityEngine;
 
+    /// <summary>
+    /// Attribute to create a drop-down list from a struct in the inspector.
+    /// </summary>
+    /// <example>
+    /// [DropDown(typeof(Directions))]
+    /// [SerializeField] private Vector2 _direction = Directions.Right;
+    /// </example>
     public class DropDownAttribute : PropertyAttribute
     {
         private readonly Type _structType;
         private FieldInfo[] _fields;
 
+        /// <summary>
+        /// Attribute to create a drop-down list from a struct in the inspector.
+        /// </summary>
+        /// <param name="structType">Type of the struct or class that holds the options for a drop-down.</param>
+        /// <example>
+        /// [DropDown(typeof(Directions))]
+        /// [SerializeField] private Vector2 _direction = Directions.Right;
+        /// </example>
         public DropDownAttribute(Type structType)
         {
             _structType = structType;
